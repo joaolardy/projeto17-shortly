f73b3e651c4550b04bac2a0331ab03399378e079
joaolardy <joaolardy@gmail.com>
2023-03-05 15:28:19 -0300

    schemas e base para as rotas prontos

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors"
 import dotenv from "dotenv"
+import routes from "./routes/index.routes";
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(routes);
 
 const port = process.env.PORT || 5001;
 app.listen(port, () => console.log(`Server is running in port ${port}`));
